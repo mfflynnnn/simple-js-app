76904a8c394e9ca589be432275f8a80a7a76e227
mfflynnnn <89746780+mfflynnnn@users.noreply.github.com>
2022-12-21 10:32:34 -0800

    Completed adding data

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
-  { name: "Squirtle", height: 0.5, types: ["water"], weight:  },
-  { name: "Pidgey", height: 0.3, types: ["flying", "normal"], weight:  },
-  { name: "Pikachu", height: 0.4, types: ["electric"], weight:  },
+  { name: "Squirtle", height: 0.5, types: ["water"], weight: 9 },
+  { name: "Pidgey", height: 0.3, types: ["flying", "normal"], weight: 1.8 },
+  { name: "Pikachu", height: 0.4, types: ["electric"], weight: 6 },
   { name: "Jigglypuff", height: 0.5, types: ["fairy", "normal"], weight: 5.5 },
-  { name: , height: , types: [], weight:  },
+  { name: "Bouffalant", height: 1.6, types: ["normal"], weight: 94.6 },
 ];
