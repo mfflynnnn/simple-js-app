3cdec4567ccfd515ead71e331d24ec3ae73d7e1a
mfflynnnn <89746780+mfflynnnn@users.noreply.github.com>
2022-12-21 10:30:22 -0800

    Updated value key in object. Added more data.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
-  { name: "Squirtle", height: 0.5, types: ["water"], evolutions: 16 },
-  { name: "Pidgey", height: 0.3, types: ["flying", "normal"], evolutions: 18 },
-  { name: , height: , types: [], evolutions:  },
-  { name: , height: , types: [], evolutions:  },
-  { name: , height: , types: [], evolutions:  },
+  { name: "Squirtle", height: 0.5, types: ["water"], weight:  },
+  { name: "Pidgey", height: 0.3, types: ["flying", "normal"], weight:  },
+  { name: "Pikachu", height: 0.4, types: ["electric"], weight:  },
+  { name: "Jigglypuff", height: 0.5, types: ["fairy", "normal"], weight: 5.5 },
+  { name: , height: , types: [], weight:  },
 ];
