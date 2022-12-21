1ab343c29ac1a916d4c3b9b579b541bb019cd998
mfflynnnn <89746780+mfflynnnn@users.noreply.github.com>
2022-12-21 10:27:14 -0800

    Altered value key in object. Added data

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
-  { name: "Squirtle", height: 0.5, types: ["water"], catchRate: 0 },
-  { name: , height: , types: [], catchRate:  },
-  { name: , height: , types: [], catchRate:  },
-  { name: , height: , types: [], catchRate:  },
-  { name: , height: , types: [], catchRate:  },
+  { name: "Squirtle", height: 0.5, types: ["water"], evolutions: 16 },
+  { name: "Pidgey", height: 0.3, types: ["flying", "normal"], evolutions: 18 },
+  { name: , height: , types: [], evolutions:  },
+  { name: , height: , types: [], evolutions:  },
+  { name: , height: , types: [], evolutions:  },
 ];
