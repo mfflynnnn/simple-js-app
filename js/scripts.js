f0a7d84203fc204868d1e1de59f2744968627674
mfflynnnn <yougotanemailfrommolly@gmail.com>
2022-12-21 10:24:21 -0800

    Created templated array of objects
    Added the values of the first object as part of the template.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+let pokemonList = [
+  { name: "Squirtle", height: 0.5, types: ["water"], catchRate: 0 },
+  { name: , height: , types: [], catchRate:  },
+  { name: , height: , types: [], catchRate:  },
+  { name: , height: , types: [], catchRate:  },
+  { name: , height: , types: [], catchRate:  },
+];
