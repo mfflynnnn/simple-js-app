cd49de7b5916fb7cd4260e539efb1f5efd4dabba
mfflynnnn <89746780+mfflynnnn@users.noreply.github.com>
2022-12-22 01:17:49 -0800

    Looped through data and printed to page

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,9 @@ let pokemonList = [
   { name: "Jigglypuff", height: 0.5, types: ["fairy", "normal"], weight: 5.5 },
   { name: "Bouffalant", height: 1.6, types: ["normal"], weight: 94.6 },
 ];
+
+//Looping through the array of objects that contain the pokemon data and printing
+//it to the page.
+for (i = 1; i < pokemonList.length; i++) {
+  document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}m).`);
+}
